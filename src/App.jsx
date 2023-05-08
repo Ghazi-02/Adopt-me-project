ed414953af80a0eb54ee512d47440742da2a7e34
Areeb ul haq Mirza <auhmirza02@gmail.com>
2023-05-08 11:48:13 +0100

    Create module fetchPet using react queryClient

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Link, BrowserRouter, Routes, Route} from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./details";
-
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries:{
+      staleTime: Infinity,
+      cacheTime:Infinity,  
+    },
+  },
+})
 const App = () => {
   return (
     <BrowserRouter>
+      <QueryClientProvider client = {queryClient}>      
       <header>
         <Link to="/">Adopt Me!</Link>
       </header>
@@ -14,7 +23,7 @@ const App = () => {
       <Routes>
         <Route path="/details/:id" element={<Details />} />
         <Route path="/" element={<SearchParams />} />
-      </Routes>
+      </Routes></QueryClientProvider>
     </BrowserRouter>
   );
 };
